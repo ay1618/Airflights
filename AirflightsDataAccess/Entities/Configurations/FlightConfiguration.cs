1b6e8376a225a988d4be32eacacb6215842c233f
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-16 15:08:57 +0600

    add entities

---
+++ AirflightsDataAccess/Entities/Configurations/FlightConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirflightsDataAccess.Entities.Configurations
+{
+    public class FlightConfiguration : IEntityTypeConfiguration<Flight>
+    {
+        public void Configure(EntityTypeBuilder<Flight> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.RegNum).IsRequired().HasMaxLength(20);
+            builder.HasIndex(x => x.RegNum).IsUnique();
+            builder.Property(x => x.FromCityId).IsRequired();
+            builder.Property(x => x.ToCityId).IsRequired();
+            builder.Property(x => x.ArrivalTime).IsRequired();
+            builder.Property(x => x.DepartureTime).IsRequired();
+            builder.Property(x => x.Created).IsRequired();
+            builder.Property(x => x.UserId).IsRequired();
+        }
+    }
+}
---
+++ AirflightsDataAccess/Entities/Flight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirflightsDataAccess.Entities
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public string RegNum { get; set; }
+        public int FromCityId { get; set; }
+        public int ToCityId { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public long Delay { get; set; }
+        public int UserId { get; set; }
+        public bool IsArchive { get; set; }
+    }
+}
