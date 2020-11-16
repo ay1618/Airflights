2a5857fb9c406efcb6742420adb84394ea6e071c
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-16 15:08:26 +0600

    add ef core & add context

---
+++ AirflightsDataAccess/ApplicationContext.cs
@@ -0,0 +1,31 @@
+﻿using AirflightsDataAccess.Entities;
+using AirflightsDataAccess.Entities.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirflightsDataAccess
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        //{
+        //    builder.UseNpgsql("Host=localhost;Port=5432;Database=firstEfDb;Username=postgres;Password=Zere707Z");
+        //}
+
+        protected override void OnModelCreating(ModelBuilder mBuilder)
+        {
+            //base.OnModelCreating(mBuilder);
+            mBuilder.ApplyConfiguration(new FlightConfiguration());
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
---
+++ AirflightsDataAccess/ExtensionMethods/DataAccessExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirflightsDataAccess.ExtensionMethods
+{
+    public static class DataAccessExtension
+    {
+        public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, string connectionString)
+        {
+            return services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+        }
+    }
+}
