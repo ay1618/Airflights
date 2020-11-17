610a3e348533a07d3447db9d53dc072dde678110
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-17 20:28:56 +0600

    added DictController

---
+++ Airflights/Controllers/DictController.cs
@@ -0,0 +1,30 @@
+﻿using AirflightsDomain.Models;
+using AirflightsDomain.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airflights.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DictController : ControllerBase
+    {
+        private readonly DictService _dictService;
+
+        public DictController(DictService dictService)
+        {
+            this._dictService = dictService;
+        }
+
+        [HttpGet("cities")]
+        public async Task<List<CityDTO>> Cities()
+        {
+            return await _dictService.GetAll();
+        }
+        
+    }
+}
