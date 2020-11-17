e7280cbe3046511fe166691ec021d5277f8b7319
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-17 20:23:32 +0600

    abstract flight repository

---
+++ AirflightsDomain/IFlightsRepository.cs
@@ -0,0 +1,38 @@
+﻿using AirflightsDomain.Models;
+using AirflightsDomain.Models.Flight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirflightsDomain
+{
+    public interface IFlightsRepository
+    {
+        /// <summary>
+        /// вытащить все
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<FlightDTO>> GetAsync();
+
+        /// <summary>
+        /// вытащить по id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<FlightDTO> GetAsync(int id);
+
+        /// <summary>
+        /// создать рейс
+        /// </summary>
+        /// <param name="flight"></param>
+        /// <returns></returns>
+        Task<int> CreateAsync(CreateFlightDTO flight);
+
+        Task UpdateAsync(UpdateFlightDTO flight);
+
+        Task DeleteAsync(int flightId);
+
+        Task AddDelayAsync(int flightId, TimeSpan delay);
+    }
+}
