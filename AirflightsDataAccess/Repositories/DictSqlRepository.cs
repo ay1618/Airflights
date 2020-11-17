1d32bd41a34756888ade9ede904fb3c1722def1f
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-17 20:28:38 +0600

    Implementation of IDictRepository

---
+++ AirflightsDataAccess/Repositories/DictSqlRepository.cs
@@ -0,0 +1,29 @@
+﻿using AirflightsDataAccess.Entities;
+using AirflightsDomain.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirflightsDataAccess.Repositories
+{
+    public class DictSqlRepository : AirflightsDomain.IDictRepository
+    {
+        private readonly ApplicationContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public DictSqlRepository(ApplicationContext dbContext, IMapper mapper)
+        {
+            this._dbContext = dbContext;
+            this._mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CityDTO>> GetAsync()
+        {
+            IEnumerable<City> flights = await _dbContext.Cities.ToListAsync();
+            return _mapper.Map<IEnumerable<CityDTO>>(flights);
+        }
+    }
+}
