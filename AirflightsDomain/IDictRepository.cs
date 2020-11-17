107fbd2d529bfadc08268928d5261af423bc511b
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-17 20:23:53 +0600

    abstract dictionary repository

---
+++ AirflightsDomain/IDictRepository.cs
@@ -0,0 +1,13 @@
+﻿using AirflightsDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirflightsDomain
+{
+    public interface IDictRepository
+    {
+        Task<IEnumerable<CityDTO>> GetAsync();
+    }
+}
