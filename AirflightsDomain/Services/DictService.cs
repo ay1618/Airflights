4acc841282991c43f479298b80b4b757f83369d8
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-17 20:24:20 +0600

    dictionary & flights services

---
+++ AirflightsDomain/Services/DictService.cs
@@ -0,0 +1,21 @@
+﻿using AirflightsDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirflightsDomain.Services
+{
+    public class DictService
+    {
+        private readonly IDictRepository _dictRepository;
+
+        public DictService(IDictRepository dictRepository)
+        {
+            this._dictRepository = dictRepository;
+        }
+
+        public async Task<List<CityDTO>> GetAll() => (await _dictRepository.GetAsync()).ToList();
+    }
+}
---
+++ AirflightsDomain/Services/FlightsService.cs
@@ -0,0 +1,21 @@
+﻿using AirflightsDomain.Models.Flight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirflightsDomain.Services
+{
+    public class FlightsService
+    {
+        private readonly IFlightsRepository _flightsRepository;
+
+        public FlightsService(IFlightsRepository flightsRepository)
+        {
+            this._flightsRepository = flightsRepository;
+        }
+
+        public async Task<List<FlightDTO>> GetAll() => (await _flightsRepository.GetAsync()).ToList();
+    }
+}
