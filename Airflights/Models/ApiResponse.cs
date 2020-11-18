cd4cd92cee584c4c1047f028b7a52ad68b977e94
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-18 15:15:08 +0600

    Response wrapper

---
+++ Airflights/Models/ApiResponse.cs
@@ -0,0 +1,29 @@
+﻿using AirflightsShared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airflights.Models
+{
+    public class ApiResponse
+    {
+        public ApiResponse(ResultCode resultCode, string message = "")
+        {
+            this.resultCode = resultCode;
+            this.Message = message;
+        }
+        public ResultCode resultCode { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ApiResponse<T> : ApiResponse
+    {
+        public ApiResponse(T data) : base(ResultCode.Ok)
+        {
+            this.Data = data;
+        }
+
+        public T Data { get; set; }
+    }
+}
