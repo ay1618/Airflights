8d3f3abf02e0f5c1e56da9df10011fa76fab5ce9
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-18 22:12:49 +0600

    automapper UserProfile

---
+++ AirflightsDataAccess/Profiles/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using AirflightsDataAccess.Entities;
+using AirflightsDomain.Models.User;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirflightsDataAccess.Profiles
+{
+    public class UserProfile: Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserDTO, User>();
+            CreateMap<User, UserDTO>();
+        }
+    }
+}
