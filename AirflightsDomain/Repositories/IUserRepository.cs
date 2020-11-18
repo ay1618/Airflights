e2e966f43d68a69d88cce231dee155684520d203
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-18 22:11:09 +0600

    UserRepository

---
+++ AirflightsDomain/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using AirflightsDomain.Models;
+using AirflightsDomain.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirflightsDomain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<UserDTO> GetAsync(string login, string password);
+
+        Task CreateAsync(CreateUserDTO user);
+    }
+}
