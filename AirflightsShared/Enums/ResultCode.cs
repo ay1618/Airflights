17f4e6ef7bc8218f1cf5459a768d1b531b516539
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-18 15:15:30 +0600

    Shared elements

---
+++ AirflightsShared/Enums/ResultCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirflightsShared.Enums
+{
+    public enum ResultCode
+    {
+        Ok = 0,
+        Error = -1,
+        ModelValidationError = -2,
+        AuthorizationError = -3
+    }
+}
