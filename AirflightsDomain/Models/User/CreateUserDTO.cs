1b18e4da8648fdbaf25be8f0325e5a2205c9500c
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-18 22:10:57 +0600

    UserDto

---
+++ AirflightsDomain/Models/User/CreateUserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirflightsDomain.Models.User
+{
+    public class CreateUserDTO
+    {
+        public String Login { get; set; }
+        public String Name { get; set; }
+        public String Password { get; set; }
+    }
+}
---
+++ AirflightsDomain/Models/User/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirflightsDomain.Models.User
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+    }
+}
