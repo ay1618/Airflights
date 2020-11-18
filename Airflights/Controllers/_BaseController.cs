936558f7a29adb679b67b8136784e94e635dd2d8
Alisher Yessenkozha <alisher1.618@gmail.com>
2020-11-18 15:16:26 +0600

    custom base controller

---
+++ Airflights/Controllers/_BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Airflights.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airflights.Controllers
+{
+    public abstract class _BaseController : ControllerBase
+    {
+        public override OkObjectResult Ok([ActionResultObjectValue] object value)
+        {
+            return base.Ok(new ApiResponse<object>(value));
+        }
+    }
+}
